ecf445231ece55881034db1b07ef589f0a633d78
raltshu <raltshu@gmail.com>
2021-08-03 18:36:08 +0300

    complete

--- src/handlers/alerts_proxy.py
+++ src/handlers/alerts_proxy.py
@@ -10,7 +10,10 @@ WARNING ='WARNING'
 ERROR = 'ERROR'
 
 TRAIN_MODEL ='TRAIN_MODEL'
+TRAIN_OUTSOURCE_MODEL ='TRAIN_OUTSOURCE_MODEL'
 MODEL_SCORE ='MODEL_SCORE'
+OUTSOURCE_MODEL_SCORE ='OUTSOURCE_MODEL_SCORE'
+LOAD_DATA_FROM_WEB = 'LOAD_DATA_FROM_WEB'
 
 class BasicAlert:
 
@@ -50,11 +53,43 @@ class AlertView(FlaskView):
     @route('/calc_score_complete', methods=['POST'])
     def model_score_complete(self):
         model_metrics = request.get_json()
-        alert = BasicAlert(INFO,MODEL_SCORE,f'Model Score completed {model_metrics}')
+        model_sevirity = INFO
+        if float(model_metrics['rmspe']) > 10:
+            model_sevirity = WARNING
+        alert = BasicAlert(model_sevirity,MODEL_SCORE,f'Model Score completed {model_metrics}')
         return alert.dispatch_to_db().text
 
     @route('/calc_score_begin', methods=['POST'])
     def model_score_start(self):
         data = request.get_json()
         alert = BasicAlert(INFO,MODEL_SCORE,'Begin model score calculation')
+        return alert.dispatch_to_db().text
+
+    @route('/train_outsource_model_start', methods=['POST'])
+    def train_outsource_model_begin(self):
+        data = request.get_json()
+        rows_number = int(data['rows_number'])
+        alert = BasicAlert(INFO,TRAIN_OUTSOURCE_MODEL,f'Train outsource model started. Using {rows_number:,d}')
+        return alert.dispatch_to_db().text
+
+    @route('/train_outsource_model_finish', methods=['POST'])
+    def train_outsource_model_complete(self):
+        model_metrics = request.get_json()
+        alert = BasicAlert(INFO,TRAIN_OUTSOURCE_MODEL,f'Train outsource model completed {model_metrics}')
+        return alert.dispatch_to_db().text
+
+    @route('/calc_outsource_score_complete', methods=['POST'])
+    def model_outsource_score_complete(self):
+        model_metrics = request.get_json()
+        model_sevirity = INFO
+        if float(model_metrics['rmspe']) > 10:
+            model_sevirity = WARNING
+        alert = BasicAlert(model_sevirity,OUTSOURCE_MODEL_SCORE,f'Model Outsource Score completed {model_metrics}')
+        return alert.dispatch_to_db().text
+
+    @route('/calc_outsource_score_begin', methods=['POST'])
+    def model_outsource_score_start(self):
+        data = request.get_json()
+        rows_number = int(data['rows_number'])
+        alert = BasicAlert(INFO,OUTSOURCE_MODEL_SCORE,f'Begin outsource model score calculation with {rows_number:,d} rows')
         return alert.dispatch_to_db().text
